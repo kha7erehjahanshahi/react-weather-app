65d7e285b46a0f33dba4d888366b9aebc1ee4b38
kha7erehjahanshahi <111133121+kha7erehjahanshahi@users.noreply.github.com>
2023-08-21 11:37:45 +0000

    Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className='header-container'>
         <h1 className='header-title'>Weather App </h1>
-        <img src='/Images/app-logo.png' alt='app-logo'/>
+        <img src='/react-weather-app/Images/app-logo.png' alt='app-logo'/>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,36 +3,36 @@ import Header from "./Header";
 import "./Home.css";
 import getWeather from "./api";
 
-
-
 function Home() {
   const [data, setData] = useState({
     celcius: 10,
     name: "Germany",
     humidity: 10,
     speed: 2,
-    image: "/Images/clouds.png",
+    image: "clouds",
   });
 
   const [name, setName] = useState("");
 
   const handleClick = () => {
     if (name === "") {
-      return
+      return;
     }
 
-    getWeather(name).then(weatherData => {
-      setData({
-        ...data,
-        celcius: weatherData.main.temp,
-        name: weatherData.name,
-        humidity: weatherData.main.humidity,
-        speed: weatherData.wind.speed,
-        image: weatherData.weather[0].main.toLowerCase(),
+    getWeather(name)
+      .then((weatherData) => {
+        setData({
+          ...data,
+          celcius: weatherData.main.temp,
+          name: weatherData.name,
+          humidity: weatherData.main.humidity,
+          speed: weatherData.wind.speed,
+          image: weatherData.weather[0].main.toLowerCase(),
+        });
+      })
+      .catch((error) => {
+        console.log(error);
       });
-    }).catch(error => {
-      console.log(error);
-    });
   };
 
   return (
@@ -47,22 +47,30 @@ function Home() {
               onChange={(e) => setName(e.target.value)}
             />
             <button>
-            <img src="/react-weather-app/Images/search.png" onClick={handleClick} alt="" />
+              <img
+                src="/react-weather-app/Images/search.png"
+                onClick={handleClick}
+                alt=""
+              />
             </button>
           </div>
           <div className="weather-info">
-          <img className="icon" src={`/react-weather-app/Images/${data.image}.png`} alt="" />
+            <img
+              className="icon"
+              src={`/react-weather-app/Images/${data.image}.png`}
+              alt=""
+            />
             <h1>{Math.round(data.celcius)}°C</h1>
             <h2>{data.name}</h2>
             <div className="details">
               <div className="col">
-              <img src="/react-weather-app/Images/humidity.png" alt="" />
+                <img src="/react-weather-app/Images/humidity.png" alt="" />
                 <div className="humidity">
                   <p>{Math.round(data.humidity)}%</p>
                   <p>Humidity</p>
                 </div>
               </div>
-              <div className="col">          
+              <div className="col">
                 <img src="/react-weather-app/Images/wind.png" alt="" />
                 <div className="wind">
                   <p>{Math.round(data.speed)} km/h</p>
