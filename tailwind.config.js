a86b1b0bb141232f29a03f317f76d9b7c82d4cf4
Ali X <al00x@outlook.com>
2023-08-27 13:49:38 +0330

    Tailwind colors extend refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#F0F8FF',
+          200: '#d8eafa',
+          300: '#93c0e7',
+          400: '#4796da',
+          500: '#2a7fc9',
+          600: '#156fbd',
+          700: '#0c4c83',
+          800: '#063257',
+          900: '#021e34'
+        },
+        surface: {
+          DEFAULT: '#8484A3',
+          light: '#DEEDF3',
+          dark: '#26263E'
+        },
+      }
+    },
   },
   plugins: [],
 }
